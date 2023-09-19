8f6b4d5a6ccd2fed7dfc140ad734ca3530050be1
anthonyfolino <folinoo_@hotmail.com>
2023-09-19 21:00:26 +1000

    feat(services page started, animations and copy complete)

--- src/views/Services/index.tsx
+++ src/views/Services/index.tsx
@@ -5,6 +5,42 @@ export const Services = (): JSX.Element => {
     return (
         <div className="services-container">
             <h1>Services</h1>
+
+            {/* <div className="image-grid">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div> */}
+
+            <div className="cards">
+                <div className="card first">
+                    <h2>Vehicle Sales</h2>
+                    <p>Discover a curated selection of the world's finest luxury automobiles. Our showroom showcases a range of prestigious brands, each meticulously chosen for its superior craftsmanship, performance, and style.</p>
+                </div>
+                <div className="card second">
+                    <h2>Personalised Consultations</h2>
+                    <p>Our team of experts is here to provide personalized guidance tailored to your unique preferences. Whether you're seeking a performance-driven sports car, a luxurious SUV, or a timeless classic, we'll assist you in finding the perfect match.</p>
+                </div>
+                <div className="card third">
+                    <h2>Trade-In and Upgrade</h2>
+                    <p>Looking to part ways with your current luxury vehicle? We offer competitive trade-in appraisals, allowing you to seamlessly transition to the latest model or a different luxury car within our collection.</p>
+                </div>
+                <div className="card fourth">
+                    <h2>Financing and Leasing</h2>
+                    <p>Our flexible financing options and leasing solutions are designed to make your dream car a reality. We work with trusted financial partners to ensure you secure the best terms and rates.</p>
+                </div>
+                <div className="card fifth">
+                    <h2>Maintenance and Servicing</h2>
+                    <p>Trust your prized possession to our certified technicians who understand the intricacies of luxury vehicles. Our state-of-the-art service center is equipped to handle everything from routine maintenance to complex repairs.</p>
+                </div>
+                <div className="card sixth">
+                    <h2>Detailing and Customisation</h2>
+                    <p>Enhance the aesthetics of your luxury car with our professional detailing and customization services. From paint protection to bespoke interiors, we'll help you personalize your vehicle to perfection.</p>
+                </div>
+            </div>
         </div>
     )
 }
