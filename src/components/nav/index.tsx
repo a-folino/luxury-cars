b02f4193cfc21510c479bb76f9c6a01bf1f632e1
anthonyfolino <folinoo_@hotmail.com>
2023-09-20 19:08:04 +1000

    chore(all pages and menu responsive)

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './styles.scss';
+import { IoMenuSharp } from 'react-icons/io5';
+import { IoClose } from 'react-icons/io5';
 
 type Props = {
     setView: (view: string) => void;
@@ -7,39 +9,63 @@ type Props = {
 }
 
 export const Nav = ({ setView, view }: Props): JSX.Element => {
+    const [menuOpen, setMenuOpen] = useState(false);
     const homeRef = useRef<HTMLDivElement>(null);
     const carsRef = useRef<HTMLDivElement>(null);
     const servicesRef = useRef<HTMLDivElement>(null);
     const aboutRef = useRef<HTMLDivElement>(null);
     const contactRef = useRef<HTMLDivElement>(null);
-
+    const mobileMenuRef = useRef<HTMLDivElement>(null);
 
     const handleClick = (e: React.MouseEvent) => {
         const target = e.target as HTMLDivElement;
         setView(target.textContent as string);
     }
 
+    const handleMenuClick = () => {
+        const menu = mobileMenuRef.current as HTMLDivElement;
+        menu.classList.toggle('menu-open');
+        setMenuOpen(!menuOpen);
+    }
+
     useEffect(() => {
         const refs = [homeRef.current, carsRef.current, servicesRef.current, aboutRef.current, contactRef.current];
 
         if (view === '') return setView('Home');
-
-        refs.forEach((ref) => {
-            if (ref?.textContent === view) {
-                ref.classList.add('active-nav');
-            } else {
-                ref?.classList.remove('active-nav');
-            }
-        })
+        if (window.innerWidth > 700) {
+            refs.forEach((ref) => {
+                if (ref?.textContent === view) {
+                    ref.classList.add('active-nav');
+                } else {
+                    ref?.classList.remove('active-nav');
+                }
+            })  
+        }
+        
     }, [view, setView])
 
     return (
-        <nav>
-            <div className="nav-item" onClick={(e) => handleClick(e)} ref={homeRef}>Home</div>
-            <div className="nav-item" onClick={(e) => handleClick(e)} ref={carsRef}>Cars</div>
-            <div className="nav-item" onClick={(e) => handleClick(e)} ref={servicesRef}>Services</div>
-            <div className="nav-item" onClick={(e) => handleClick(e)} ref={aboutRef}>About</div>
-            <div className="nav-item" onClick={(e) => handleClick(e)} ref={contactRef}>Contact</div>
-        </nav>
+        <>
+        {window.innerWidth < 700 ?
+            <div className="hamburger-menu" onClick={() => handleMenuClick()}>
+                {!menuOpen ? <IoMenuSharp/> : <IoClose /> }
+            </div> :
+            <nav>
+                <div className="nav-item" onClick={(e) => handleClick(e)} ref={homeRef}>Home</div>
+                <div className="nav-item" onClick={(e) => handleClick(e)} ref={carsRef}>Cars</div>
+                <div className="nav-item" onClick={(e) => handleClick(e)} ref={servicesRef}>Services</div>
+                <div className="nav-item" onClick={(e) => handleClick(e)} ref={aboutRef}>About</div>
+                <div className="nav-item" onClick={(e) => handleClick(e)} ref={contactRef}>Contact</div>
+            </nav>
+        }
+
+        <div className="hamburger-menu-open" onClick={() => handleMenuClick()} ref={mobileMenuRef}>
+            <div className="mobile-menu-item" onClick={(e) => handleClick(e)} ref={homeRef}>Home</div>
+            <div className="mobile-menu-item" onClick={(e) => handleClick(e)} ref={carsRef}>Cars</div>
+            <div className="mobile-menu-item" onClick={(e) => handleClick(e)} ref={servicesRef}>Services</div>
+            <div className="mobile-menu-item" onClick={(e) => handleClick(e)} ref={aboutRef}>About</div>
+            <div className="mobile-menu-item" onClick={(e) => handleClick(e)} ref={contactRef}>Contact</div>
+        </div>
+        </>
     )
 }
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -8,9 +8,6 @@ export const Home = (): JSX.Element => {
             </video>
             <div className="overlay">
                 <img src={require('../../assets/logo.png')} alt="logo" className="logo" />
-                {/* <h1 ref={titleRef}>Unparalleled Luxury</h1>
-                <h3>At Auto Emporium Melbourne, we redefine automotive excellence. Our exclusive selection of luxury cars represents the pinnacle of craftsmanship, performance, and sophistication.</h3> */}
-                {/* <img src={require('../../assets/ferrari.webp')} alt="ferrari" className="ferrari" /> */}
             </div>
         </div>
     )
